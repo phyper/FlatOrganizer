623605b05b43eed24eafccc07698a9f096c99801
phyper <benjamin.behm@aalto.fi>
2013-02-07 22:31:10 +0000

    Updated: models.py -> import PIL from Image

--- flats/models.py
+++ flats/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 import datetime
-
-# Create your models here.
+from PIL import Image
 
 class UserProfile (models.Model):
 	user = models.OneToOneField(User)
@@ -11,7 +10,7 @@ class UserProfile (models.Model):
 	email = models.EmailField()
 	num_stars = models.IntegerField()
 	amount_of_points = models.IntegerField(editable=False)
- 	avatar = models.ImageField(upload_to='images');
+ 	avatar = models.ImageField(upload_to='images')
 	
 	#def __unicode__(self):
      #   return self.user.username
@@ -21,7 +20,7 @@ class Person(models.Model):
 	last_name = models.CharField(max_length=50)
 	email = models.EmailField()
 	num_stars = models.IntegerField()
-	amount_of_points = models.IntegerField(editable=False); # can't edit this
+	amount_of_points = models.IntegerField(editable=False) # can't edit this
 	
 	def __unicode__(self):
 		return self.first_name
@@ -30,24 +29,34 @@ class Flat(models.Model):
 	persons = models.ManyToManyField(Person) # each person lives in a Flat (or several flats -> how to do)
 	name = models.CharField(max_length=30)
 	description = models.CharField(max_length=100)
+	active = models.BooleanField() # a flat can be in use or not
 	
 	def __unicode__(self):
 		return self.name
 	
 class Task(models.Model):
-	person = models.ForeignKey(Person) # each Task is related to a single Person
+	#person = models.ForeignKey(Person) # each Task is related to a single Person
 	name = models.CharField(max_length=50)
 	description = models.CharField(max_length=100)
 	credits = models.IntegerField() # try to limit value between 1 and 10
-	creation_date = models.DateTimeField(editable=False)
-	due_date = models.DateTimeField(editable=True)
-	completion_date = models.DateTimeField(editable=False)
+	#category = models.OneToOneField(Category)
+	#creation_date = models.DateTimeField(editable=False)
+	#due_date = models.DateTimeField(editable=True)
+	#completion_date = models.DateTimeField(editable=False)
 	
 	def __unicode__(self):
 		return self.name
 	
 #class Assigned_Task(models.Model):
-#	person = models.ForeignKey(Person)
+#	task = models.OneToOneField(Person)
+#	person = models.OneToOneField(Person)
+#	flat = models.OneToOneField(Person)
 #	creation_date = models.DateTimeField(editable=False)
 #	due_date = models.DateTimeField(editable=True)
+#	completion_date = models.DateTimeField(editable=False)
+	
+class Category(models.Model):
+	name = models.CharField(max_length=100)
 		
+	def __unicode__(self):
+		return self.name
