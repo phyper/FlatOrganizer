ab494206038af1c490e8c66b38fc90ee00f1542c
Haakon Sonsteby <haakon.sonsteby@gmail.com>
2013-02-27 13:37:06 +0000

    added support for flat view

--- flats/urls.py
+++ flats/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     url(r'^login/$', views.user_login, name='login'),
     url(r'^logout/$', views.user_logout, name='logout'),
     url(r'^profile/', views.profile, name='profile'),
-    url(r'^password_change/', views.password_change, name='password_change')
+    url(r'^password_change/', views.password_change, name='password_change'),
+    url(r'^flat/', views.flat, name='flat'),
     
 )
